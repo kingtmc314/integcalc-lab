6bfcfb0fa80101dc232e20c431276db44a4fc195
IntegCalc Deploy <deploy@integcalc.lab>
2026-05-16 08:39:44 +0000

    feat: add Canvas 2D solid-of-revolution fallback for no-WebGL devices

--- assets/Solid3D-CZG0R6_V.js
+++ assets/Solid3D-CZG0R6_V.js
@@ -1,4 +1,4 @@
-import{r as Se,e as Ea,j as se,a as ya}from"./index-Cw1wuIAy.js";/**
+import{r as Se,e as Ea,j as se,a as ya}from"./index-CGIYluLV.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
